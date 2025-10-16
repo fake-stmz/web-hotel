6a42de88063551e822f0e4947fab38ef3275188b
student-pro <student-pro@b209-15.khpk.ru>
2025-10-16 09:19:39 +0700

    Added auth pages templates

--- hotel_business/hotel/views.py
+++ hotel_business/hotel/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
